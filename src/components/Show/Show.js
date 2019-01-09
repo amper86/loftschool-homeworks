f9b0797ca93acd629d469a59cc792b69cda4e274
Oleg Mullakaev <o.mullakaev@gmail.com>
2019-01-09 23:17:04 +0300

    render refactoring

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -27,30 +27,47 @@ class Show extends Component {
     }
   }
 
-  render() {
+  noShowRender = () => {
+    return (<p className='show-information t-show-info'>Шоу не выбрано</p>);
+  };
+
+  preloaderRender = () => {
+    return (<p className='show-information t-show-info'>Загрузка!!!</p>);
+  };
+
+  showRender = () => {
+    const { data } = this.state;
+    return (
+      <div className='show'>
+        <img
+          className='show-image'
+          src={data.image.medium}
+          alt={data.name}/>
+        <h2 className='show-label t-show-name'>{data.name}</h2>
+        <p className='show-text t-show-genre'>
+          <b>Жанр: </b>{data.genres.join(', ')}
+        </p>
+        <p
+          className='show-text t-show-summary'
+          dangerouslySetInnerHTML={{ __html: data.summary }}/>
+      </div>
+    )
+  };
+
+  showContent = () => {
     const {showId, data} = this.state;
 
     if (!showId) {
-      return (<p className='show-information t-show-info'>Шоу не выбрано</p>)
+      return this.noShowRender()
     } else if (Object.keys(data).length === 0) {
-      return (<p className='show-information t-show-info'>Загрузка!!!</p>)
+      return this.preloaderRender()
     } else {
-      return (
-        <div className='show'>
-          <img
-            className='show-image'
-            src={data.image.medium}
-            alt={data.name}/>
-          <h2 className='show-label t-show-name'>{data.name}</h2>
-          <p className='show-text t-show-genre'>
-            <b>Жанр: </b>{data.genres.join(', ')}
-          </p>
-          <p
-            className='show-text t-show-summary'
-            dangerouslySetInnerHTML={{ __html: data.summary }}/>
-        </div>
-      )
+      return this.showRender()
     }
+  };
+
+  render() {
+    return this.showContent();
   }
 }
 
