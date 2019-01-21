ec9bed04cf433008bea74310385147dba2c925b6
Oleg Mullakaev <o.mullakaev@gmail.com>
2019-01-21 22:58:27 +0300

    5 done

--- src/HOCs/withLocalstorage/withLocalstorage.js
+++ src/HOCs/withLocalstorage/withLocalstorage.js
@@ -1,6 +1,38 @@
 import React, { Component } from 'react';
 import { load, save } from '../../localstorage';
 
-const withLocalstorage = () => () => {};
+const withLocalstorage = (localstorageKey, initValue) => (WrappedComponent) => {
+  class Wrapper extends Component {
+    saveData = data => {
+      save(localstorageKey, data);
+      this.forceUpdate();
+    };
+
+    loadData() {
+      return load(localstorageKey) || initValue;
+    }
+
+    render() {
+      const {forwaredRef, ...rest} = this.props;
+
+      //console.log(forwaredRef);
+      //console.log(...rest);
+      //console.log(this.props);
+
+      return (
+        <WrappedComponent
+          {...rest}
+          ref={forwaredRef}
+          savedData={this.loadData()}
+          saveData={this.saveData}
+        />
+      )
+    }
+  }
+
+  return React.forwardRef((props, ref) => (
+    <Wrapper {...props} forwaredRef={ref} />
+  ));
+};
 
 export default withLocalstorage;
